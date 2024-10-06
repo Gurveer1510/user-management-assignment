68a8428ef9243769fbce7bd62f4d0a734a615637
Gurveer <singhgurveer4691@gmail.com>
2024-10-07 03:55:04 +0530

    fix confirm modal

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -8,7 +8,7 @@ const ConfirmModal : React.FC<ConfirmModalProps> = ({
 }) => {
 
     return (
-        <div className='fixed inset-0 bg-black bg-opacity-65 flex items-center justify-center p-4'>
+        <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4'>
             <div className='p-4 bg-white flex flex-col gap-y-2 rounded-lg max-w-xl w-full'>
                 <h2 className='text-xl font-semibold'>Are you sure ?</h2>
                 <div className='flex flex-row-reverse gap-x-2'>
@@ -18,8 +18,8 @@ const ConfirmModal : React.FC<ConfirmModalProps> = ({
                     >
                         Delete
                     </button>
-                    <button className='font-semibold border border-black rounded-lg px-2 py-1'>
-                        cancel
+                    <button className='font-semibold rounded-lg bg-slate-200 px-2 py-1'>
+                        Cancel
                     </button>
                     
                 </div>
